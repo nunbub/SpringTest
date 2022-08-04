a7e615417ebb4db4704657fee6f4e05d4717418d
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-08-04 18:48:29 +0900

    [류지수] - config 설정

--- src/main/java/com/nunbub/test/SpringTestApplication.java
+++ src/main/java/com/nunbub/test/SpringTestApplication.java
@@ -14,22 +14,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
-@MapperScan(basePackages="com.nunbub.test.*")
+
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
 	
-	@Bean
-	public SqlSessionFactory slqSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-		
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-		sessionFactory.setMapperLocations(res);
-		
-		return sessionFactory.getObject();
-	}
+	
 
 }
